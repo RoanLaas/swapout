c7cc22b56321697c9f8fdb37208029068219d448
Roan Laäs <roanlaas@gmail.com>
2025-01-26 21:35:04 +1300

    Added map page

--- swap-out/src/routes/+page.svelte
+++ swap-out/src/routes/+page.svelte
@@ -1,3 +1,3 @@
-<h1>Welcome to your library project</h1>
-<p>Create your package using @sveltejs/package and preview/showcase your work with SvelteKit</p>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<h1>Swap Out</h1>
+<p>brug</p>
+<p> <a href = "/map">mappy </a></p>
