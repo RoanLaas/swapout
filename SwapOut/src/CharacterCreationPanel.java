68893e49fe3a8a1c8acb14028ce00e8fc031578f
Roan Laäs <roanlaas@gmail.com>
2026-02-01 18:51:12 +1300

    Char creation panel

---
+++ SwapOut/src/CharacterCreationPanel.java
@@ -0,0 +1,5 @@
+import javax.swing.JPanel;
+
+public class CharacterCreationPanel extends JPanel{
+    
+}
--- SwapOut/src/CharacterSheetPanel.java
+++ SwapOut/src/CharacterSheetPanel.java
@@ -1,13 +1,31 @@
 import javax.swing.JPanel;
 import javax.swing.JLabel;
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 public class CharacterSheetPanel extends JPanel{
-    private BaseStats testBaseStats = new BaseStats(8, 7, 7, 9, 11, 1);
-    private Character testPlayer = new Character(testBaseStats, 10, "Borb2");
-
-    private int test = testPlayer.baseStats.getCharisma();
+    private JPanel mainPanel;
+    private CardLayout cardLayout;
 
     public CharacterSheetPanel() {
-        add(new JLabel(String.format("%d", test)));
+        cardLayout = new CardLayout();
+        mainPanel = new JPanel(cardLayout);
+        
+        mainPanel.add(new CharacterCreationPanel(), "CHARCREATION");
+        
+        //ToDo figure out how to align these properly to make it look nicer
+        JButton createCharButton = new JButton("Create a character");
+        createCharButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e){
+                cardLayout.show(mainPanel, "CHARCREATION");
+            }
+        });
+        mainPanel.add(createCharButton);
+
+        JButton importCharButton = new JButton("Import a character");
+        mainPanel.add(importCharButton);
     }
 }
